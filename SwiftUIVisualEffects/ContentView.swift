8bfdcac07632ed0efc73f8923427e33cc8ef59c7
mculha <melih.culha.09@gmail.com>
2024-07-01 23:39:31 +0300

    Initial Commit

---
+++ SwiftUIVisualEffects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIVisualEffects
+//
+//  Created by Melih Çulha on 1.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIVisualEffects/SwiftUIVisualEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVisualEffectsApp.swift
+//  SwiftUIVisualEffects
+//
+//  Created by Melih Çulha on 1.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVisualEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
